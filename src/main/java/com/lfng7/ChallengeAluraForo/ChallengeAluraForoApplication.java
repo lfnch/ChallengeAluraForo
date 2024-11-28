c9695ae55d756c194394a52b6fdea973d287a31f
Felipe Nieves <lfnch@outlook.com>
2024-11-28 17:32:25 -0500

    Inicio del proyecto

---
+++ src/main/java/com/lfng7/ChallengeAluraForo/ChallengeAluraForoApplication.java
@@ -0,0 +1,13 @@
+package com.lfng7.ChallengeAluraForo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeAluraForoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeAluraForoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lfng7/ChallengeAluraForo/ChallengeAluraForoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfng7.ChallengeAluraForo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAluraForoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
